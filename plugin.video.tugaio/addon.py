d03e86151bace845f927fd7a17313fe1cb1788b3
Eduardo Pires <eduardomgpires@gmail.com>
2015-07-30 23:00:20 +0100

    Versão 0.0.3 do Tuga.io
    Funcionalidades:
    - Download de Filme ou Series
    - Escolher tipo de Vistas nas configs do Addon
    - Escolher a pasta para guardar downloads nas configs do addon

--- plugin.video.tugaio/addon.py
+++ plugin.video.tugaio/addon.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python
+#Agredecimentos ao Enen92 e Manfarricos
 
-import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmcaddon,xbmc
+
+import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmcaddon,xbmc,os
 from bs4 import BeautifulSoup
+from resources.lib import Downloader #Enen92 class, muchas gracias :D
 
 addon_id    = xbmcaddon.Addon().getAddonInfo("id")
 selfAddon	= xbmcaddon.Addon(addon_id)
 addonfolder	= selfAddon.getAddonInfo('path')
 getSetting	= xbmcaddon.Addon().getSetting
-artfolder	= addonfolder + '/resources/img/'
-fanart 		= addonfolder + 'fundo.png'
+artfolder	= os.path.join(addonfolder,'resources','img')
+fanart 		= os.path.join(addonfolder,'fundo.png')
+
 
 site = 'http://tuga.io/'
 sitekids = 'http://kids.tuga.io/'
 
 def categorias():
 	if getSetting("pref_site") == 'Geral' or getSetting("pref_site") == 'Ambos':
-		addDir('Filmes', site, 1, artfolder+'filmes.png', 0)
-		addDir('Series', site, 2, artfolder+'series.png', 0)
-		addDir('Pesquisa', site, 6, artfolder+'pesquisa.png', 0)
-		addDir('', '', '', artfolder+'nada.png', 0)
+		addDir('Filmes', site, 1, os.path.join(artfolder,'filmes.png'), 0)
+		addDir('Series', site, 2, os.path.join(artfolder,'series.png'), 0)
+		addDir('Pesquisa', site, 6, os.path.join(artfolder,'pesquisa.png'), 0)
+		if "confluence" in xbmc.getSkinDir(): addDir('', '', '', os.path.join(artfolder,'nada.png'), 0)
 	if getSetting("pref_site") == 'Kids' or getSetting("pref_site") == 'Ambos':
-		addDir('Filmes KIDS', sitekids, 1, artfolder+'filmes_kids.png', 0)
-		addDir('Pesquisa KIDS', sitekids, 6, artfolder+'pesquisa_kids.png', 0)
-	xbmc.executebuiltin("Container.SetViewMode(50)")
+		addDir('Filmes KIDS', sitekids, 1, os.path.join(artfolder,'filmes_kids.png'), 0)
+		addDir('Pesquisa KIDS', sitekids, 6, os.path.join(artfolder,'pesquisa_kids.png'), 0)
+	#setVista('menu')
+	vista_menu()
 
 def getFilmes(url, pagina):
 
@@ -63,17 +68,18 @@ def getFilmes(url, pagina):
 		pagina+=1
 		for proximo in match_prox:
 			proximo = proximo[1:]
-			addDir('Proximo >>', siteAux+proximo, 1, artfolder+'proximo.png', pagina)
+			addDir('Proximo >>', siteAux+proximo, 1, os.path.join(artfolder,'proximo.png'), pagina)
 		
 	elif pagina>=1:
 		match_prox_ant=re.compile('<a href=\".+?\" class="l"><i class="fa fa-arrow-left"></i> Anterior</a><a href="(.+?)" class="r">Pr').findall(codigo_fonte_listaFilmes)
 		pagina+=1
 		for proximo in match_prox_ant:
 			proximo = proximo[1:]
-			addDir('Proximo >>', siteAux+proximo, 1, artfolder+'proximo.png', pagina)
+			addDir('Proximo >>', siteAux+proximo, 1, os.path.join(artfolder,'proximo.png'), pagina)
 	
 	mensagemprogresso.close()
-	xbmc.executebuiltin("Container.SetViewMode(500)")
+	#setVista('filmesSeries')
+	vista_filmesSeries()
 
 def getSeries(url, pagina):
 	codigo_fonte_listaSeries = abrir_url(url+'series')
@@ -93,8 +99,9 @@ def getSeries(url, pagina):
 		pagina+=1
 		for proximo in match_prox_ant:
 			proximo = proximo[1:]
-			addDir('Proximo >>', site+proximo, 2, artfolder+'proximo.png', pagina)
-	xbmc.executebuiltin("Container.SetViewMode(500)")
+			addDir('Proximo >>', site+proximo, 2, os.path.join(artfolder,'proximo.png'), pagina)
+	#setVista('filmesSeries')
+	vista_filmesSeries()
 
 def getSeasons(url):
 	codigo_fonte = abrir_url(url)
@@ -102,9 +109,10 @@ def getSeasons(url):
 	soup = BeautifulSoup(codigo_fonte)
 	
 	for temporada in soup.findAll('h2'):
-		addDirSeason(temporada.text, url, 5, artfolder+'/temporadas/temporada'+str(temporadaN+1)+'.png', 0, temporadaN)
+		addDirSeason(temporada.text, url, 5, os.path.join(artfolder,'temporadas','temporada'+str(temporadaN+1)+'.png'), 0, temporadaN)
 		temporadaN+=1
-	xbmc.executebuiltin("Container.SetViewMode(500)")
+	#setVista('temporadas')
+	vista_temporadas()
 
 
 def getEpisodes(url, temporadaNumero):
@@ -124,7 +132,8 @@ def getEpisodes(url, temporadaNumero):
 		for streamAux in getStream: stream = streamAux
 		for legendaAux in getLegenda: legenda = legendaAux 
 		addVideo(nome, stream, 3, site+imagem, site+legenda)
-	xbmc.executebuiltin("Container.SetViewMode(500)")
+	#setVista('episodios')
+	vista_episodios()
 
 def pesquisa(url):
 	siteAux = ''
@@ -140,7 +149,7 @@ def pesquisa(url):
 	if(teclado.isConfirmed()):
 		strPesquisa = teclado.getText()
 		
-		codigo_fonte_pesquisa = abrir_url_pesquisa(url, strPesquisa)
+		codigo_fonte_pesquisa = abrir_url(url, strPesquisa)
 		soup = BeautifulSoup(codigo_fonte_pesquisa)
 		filmes_series = soup.findAll('div', attrs={'class':'list'})
 
@@ -166,23 +175,102 @@ def pesquisa(url):
 			for link,imagem,nome,ano,imdb in match_series:
 				link = link[1:]
 				addDir(nome + ' ('+ano+')', siteAux+link, 4, siteAux+imagem, pagina)
-		xbmc.executebuiltin("Container.SetViewMode(500)")
+		
+		#setVista('filmesSeries')
+		vista_filmesSeries()
+
+def download(url,name,legenda):
+	folder = selfAddon.getSetting('pastaDownloads')
+
+	urlAux = clean(url.split('/')[-1])
+	legendaAux = clean(legenda.split('/')[-1])
+
+	extensaoMedia = clean(urlAux.split('.')[-1])
+	extensaoLegenda = clean(legendaAux.split('.')[1])
+
+	nomeMedia = name+'.'+extensaoMedia
+	nomeLegenda = name+'.'+extensaoLegenda
+
+
+	Downloader.Downloader().download( os.path.join(folder,nomeMedia), url, name)
+	download_legendas(legenda, os.path.join(folder,nomeLegenda))
+
+def download_legendas(url,path):
+        contents = abrir_url(url)
+        if contents:
+                fh = open(path, 'w')
+                fh.write(contents)
+                fh.close()
+        return
+
+def getInfoIMDB(id):
+	return True
+
+def vista_menu():
+	opcao = selfAddon.getSetting('menuView')
+	if opcao == '0': xbmc.executebuiltin("Container.SetViewMode(50)")
+	elif opcao == '1': xbmc.executebuiltin("Container.SetViewMode(51")
+
+def vista_filmesSeries():
+	opcao = selfAddon.getSetting('filmesSeriesView')
+	if opcao == '0': xbmc.executebuiltin("Container.SetViewMode(50)")
+	elif opcao == '1': xbmc.executebuiltin("Container.SetViewMode(51)")
+	elif opcao == '2': xbmc.executebuiltin("Container.SetViewMode(500)")
+	elif opcao == '3': xbmc.executebuiltin("Container.SetViewMode(501)")
+	elif opcao == '4': xbmc.executebuiltin("Container.SetViewMode(508)")
+	elif opcao == '5': xbmc.executebuiltin("Container.SetViewMode(504)")
+	elif opcao == '6': xbmc.executebuiltin("Container.SetViewMode(503)")
+	elif opcao == '7': xbmc.executebuiltin("Container.SetViewMode(515)")
+
+def vista_temporadas():
+	opcao = selfAddon.getSetting('temporadasView')
+	if opcao == '0': xbmc.executebuiltin("Container.SetViewMode(50)")
+	elif opcao == '1': xbmc.executebuiltin("Container.SetViewMode(51)")
+	elif opcao == '2': xbmc.executebuiltin("Container.SetViewMode(500)")
+
+def vista_episodios():
+	opcao = selfAddon.getSetting('episodiosView')
+	if opcao == '0': xbmc.executebuiltin("Container.SetViewMode(50)")
+	elif opcao == '1': xbmc.executebuiltin("Container.SetViewMode(51)")
+	elif opcao == '2': xbmc.executebuiltin("Container.SetViewMode(500)")
+
+"""def setVista(vista):
+	if vista == 'menu':
+		opcao = selfAddon.getSetting('menuView')
+		if opcao == '0': xbmc.executebuiltin("Container.SetViewMode(50)")
+		elif opcao == '1': xbmc.executebuiltin("Container.SetViewMode(51)")
+    elif vista == 'filmesSeries':
+    	opcao = selfAddon.getSetting('filmesSeriesView')
+    	if opcao == '0': xbmc.executebuiltin("Container.SetViewMode(50)")
+    	elif opcao == '1': xbmc.executebuiltin("Container.SetViewMode(51)")
+    	elif opcao == '2': xbmc.executebuiltin("Container.SetViewMode(500)")
+    	elif opcao == '3': xbmc.executebuiltin("Container.SetViewMode(501)")
+    	elif opcao == '4': xbmc.executebuiltin("Container.SetViewMode(508)")
+    	elif opcao == '5': xbmc.executebuiltin("Container.SetViewMode(504)")
+    	elif opcao == '6': xbmc.executebuiltin("Container.SetViewMode(503)")
+    	elif opcao == '7': xbmc.executebuiltin("Container.SetViewMode(515)")
+    elif vista == 'temporadas':
+    	opcao = selfAddon.getSetting('temporadasView')
+    	if opcao == '0': xbmc.executebuiltin("Container.SetViewMode(50)")
+    	elif opcao == '1': xbmc.executebuiltin("Container.SetViewMode(51)")
+    	elif opcao == '2': xbmc.executebuiltin("Container.SetViewMode(500)")
+    elif vista == 'episodios':
+    	opcao = selfAddon.getSetting('episodiosView')
+    	if opcao == '0': xbmc.executebuiltin("Container.SetViewMode(50)")
+    	elif opcao == '1': xbmc.executebuiltin("Container.SetViewMode(51)")
+    	elif opcao == '2': xbmc.executebuiltin("Container.SetViewMode(500)")
+"""
+    
 
 ###################################################################################
 #                               FUNCOES JA FEITAS                                 #
 ###################################################################################
 
-def abrir_url(url):
-	req = urllib2.Request(url)
-	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-	response = urllib2.urlopen(req)
-	link=response.read()
-	response.close()
-	return link
-
-def abrir_url_pesquisa(url,pesquisa):
-	data = urllib.urlencode({'procurar' : pesquisa})
-	req = urllib2.Request(url,data)
+def abrir_url(url,pesquisa=False):
+	if pesquisa:
+		data = urllib.urlencode({'procurar' : pesquisa})
+		req = urllib2.Request(url,data)
+	else: req = urllib2.Request(url)
 	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
 	response = urllib2.urlopen(req)
 	link=response.read()
@@ -224,9 +312,12 @@ def addDirSeason(name,url,mode,iconimage,pagina,temporada):
 def addVideo(name,url,mode,iconimage,legenda):
 	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&legenda="+urllib.quote_plus(legenda)+"&iconimage="+urllib.quote_plus(iconimage)
 	ok=True
+	contextMenuItems = []
 	liz=xbmcgui.ListItem(name, iconImage="fanart.jpg", thumbnailImage=iconimage)
 	liz.setProperty('fanart_image', iconimage)
 	liz.setInfo( type="Video", infoLabels={ "Title": name } )
+	contextMenuItems.append(('Download', 'XBMC.RunPlugin(%s?mode=7&name=%s&url=%s&iconimage=%s&legenda=%s)'%(sys.argv[0],urllib.quote_plus(name), urllib.quote_plus(url), urllib.quote_plus(iconimage), urllib.quote_plus(legenda))))
+	liz.addContextMenuItems(contextMenuItems, replaceItems=False)
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
 	return ok
 
@@ -310,4 +401,5 @@ elif mode==3: player(name, url, iconimage, legenda)
 elif mode==4: getSeasons(url)
 elif mode==5: getEpisodes(url, temporada)
 elif mode==6: pesquisa(url)
+elif mode==7: download(url, name, legenda)
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
